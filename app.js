fcdf8babf580fb22e8a90b94d68b306780627a42
Alexis Araujo <ala1289@gmail.com>
2018-11-21 07:06:10 -0600

    Respnder mensajes con messenger

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const access_token = "EAAD39wZC2ZAl8BAJcMTPix4XEQMq6UHXxTAkAf0WV0KuD8n3uUgXmeLzxzUSVPUo4shKOHyqIKwhlGmwF3MpBionDvDSvAxjNVkbDmXPUCw26nOkHsVurKCyRAwnR7CawPseC7IXcaDEZBFnbzceEvILGrhfmqpjg3Hx1qyTQZDZD"
 
 const app = express()
 
@@ -25,11 +26,43 @@ app.post('/webhook/', function(req, res){
   const webhook_event = req.body.entry[0]
   if (webhook_event.messaging) {
     webhook_event.messaging.forEach(event => {
-      console.log(event);
+      handleMessage(event)
     })
   }   
   res.sendStatus(200)
 })
+
+function handleMessage(event) {
+  const senderId = event.sender.id
+  const messageText = event.message.text
+  const messageData = {
+    recipient: {
+      id: senderId
+    },
+    message: {
+      text: messageText
+    }
+  }
+  callSendApi(messageData)
+}
+
+function callSendApi(response) {
+  request({
+    "uri": "https://graph.facebook.com/me/messages/",
+    "qs":{
+      "access_token": access_token
+    },
+    "method": "POST",
+    "json": response
+  },
+  function(err) {
+    if (err) {
+      console.log('Ha ocurrido un error');
+    } else {
+      console.log("Mensaje enviado");
+    }
+  })
+}
 
 app.listen(app.get('port'), function () {
   console.log('Nuestro servidor esta funcionando en el puerto', app.get('port'));
